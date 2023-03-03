e6956ae3f7fb0f43e06383fadb5f2a9287d49acd
unknown <caroline.daniel@showtime.net>
2023-03-02 19:06:52 -0500

    final

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,17 @@ module.exports = {
     title: `MyNewSite`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-google-gtag"]
+  plugins: ["gatsby-plugin-google-gtag",
+            "gatsby-plugin-image",
+            "gatsby-plugin-sharp",
+            {
+              resolve: "gatsby-source-filesystem",
+              options: {
+                name: `blog`,
+                path: `${__dirname}/blog`,
+              }
+            },
+            "gatsby-plugin-mdx",
+            "gatsby-transformer-sharp"          
+          ]
 };
